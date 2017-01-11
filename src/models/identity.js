880a803e77f06a119753cd4d585d4e1ffe248538
Javier Fernandez <javier.fernandez@insided.com>
2017-01-11 17:20:25 +0100

    Changed model from user to identity

---
+++ src/models/identity.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var IdentitySchema = new mongoose.Schema({
+  yip_id: {type: String, required: true},
+	profile: {type: mongoose.Schema.Types.mixed },
+	access_token: {type: String, required: true},
+	refresh_token: {type: String, required: true}
+});
+
+module.exports = mongoose.model('Identity', IdentitySchema);
