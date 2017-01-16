a9177c6d1040d8a99d533c27d299802d127aead3
luis.grande <luis.grande@insided.com>
2017-01-16 12:59:48 +0100

    add strategy routes

--- src/models/config.js
+++ src/models/config.js
@@ -5,6 +5,8 @@ var Promise = require('bluebird');
 
 var Strategy = require('../models/strategy');
 
+var DEFAULT_FIELD_TYPE = 'String';
+
 var ConfigSchema = new mongoose.Schema({
   yip_id: {type: String, required: true, unique: true},
   strategies: {}
@@ -30,7 +32,34 @@ function findByCustomer (customer) {
   });
 }
 
+function mergeCustomerStrategies (customer, strategies) {
+  return this.findByCustomer(customer).then(function (config) {
+    if (!config) {
+      return strategies;
+    }
+    var configuredStrategies = {};
+    for (var i = 0, l = config.strategies.length; i < l; i++) {
+      var strategy = config.strategies[i];
+      configuredStrategies[strategy.provider] = strategy.config;
+    }
+    for (var i = 0, l = strategies.length; i < l; i++) {
+      var strategy = strategies[i];
+      var configuredStrategy = configuredStrategies[strategy.provider];
+      var mergedFields = strategy.fields.map(function (field) {
+        return {
+          name: field.name,
+          type: field.type || DEFAULT_FIELD_TYPE,
+          value: configuredStrategy[field.name]
+        };
+      });
+      strategy.fields = mergedFields;
+    }
+    return strategies;
+  });
+}
+
 ConfigSchema.statics.validateStrategies = validateStrategies;
 ConfigSchema.statics.findByCustomer = findByCustomer;
+ConfigSchema.statics.mergeCustomerStrategies = mergeCustomerStrategies;
 
 module.exports = mongoose.model('Config', ConfigSchema);
