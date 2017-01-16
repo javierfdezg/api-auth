9b75f580f797d871b8b9adc1d62abf279b177b90
luis.grande <luis.grande@insided.com>
2017-01-16 15:23:27 +0100

    add strategy management endpoints

--- src/controllers/strategy.js
+++ src/controllers/strategy.js
@@ -17,6 +17,71 @@ function get (req, res, next) {
   }).catch(next);
 }
 
+//TODO::PLEASE PLEASE REMOVE ME :) :)
+function post (req, res, next) {
+  var strategy1 = new Strategy();
+  strategy1.provider = 'facebook';
+  strategy1.fields = [
+    {
+      name: 'callbackURL',
+      type: 'String'
+    },
+    {
+      name: 'clientSecret',
+      type: 'String'
+    },
+    {
+      name: 'clientID',
+      type: 'String'
+    }
+  ];
+
+  var strategy2 = new Strategy();
+  strategy2.provider = 'openidconnect';
+  strategy2.fields = [
+    {
+      name: 'callbackURL',
+      type: 'String'
+    },
+    {
+      name: 'clientSecret',
+      type: 'String'
+    },
+    {
+      name: 'clientID',
+      type: 'String'
+    },
+    {
+      name: 'tokenURL',
+      type: 'String'
+    },
+    {
+      name: 'userInfoURL',
+      type: 'authorizationURL'
+    },
+    {
+      name: 'issuer',
+      type: 'String'
+    }
+  ];
+  strategy1.save().then(function () {
+    console.log('facebook saved');
+    strategy2.save().then(function () {
+      console.log('openidconnect saved');
+      res.json();
+    }).catch(next);
+  }).catch(next);
+}
+
+//TODO::PLEASE PLEASE REMOVE ME :) :)
+function del (req, res, next) {
+  Strategy.remove().then(function () {
+    return res.json();
+  }).catch(next);
+}
+
 strategyController.get = get;
+strategyController.post = post;
+strategyController.del = del;
 
 module.exports = strategyController;
